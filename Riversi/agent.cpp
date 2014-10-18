2cfce079b3c5d322b9c885cdc5f5874a630de548
Varun Varadarajan <varun@Varuns-MacBook-Pro.local>
2014-10-17 18:11:09 -0700

    Renamed main.cpp to agent.cpp Rewrote the main function

---
+++ Riversi/agent.cpp
@@ -0,0 +1,43 @@
+//
+//  main.cpp
+//  Riversi
+//
+//  Created by Varun Varadarajan on 10/10/14.
+//  Copyright (c) 2014 Varun Varadarajan. All rights reserved.
+//
+
+#include <iostream>
+#include "Input.h"
+#include "Game.h"
+#include "AdversialSearchAlgorithms.h"
+#include "Output.h"
+
+int main(int argc, const char * argv[])
+{
+    Input input = *new Input();
+    Game game = *new Game(input.GetPlayer(), input.GetOpponentPlayer(), input.GetBoard(), input.GetCutOffDepth());
+    switch (input.GetSearchAlgorithm())
+    {
+        case 1:
+        {
+            State nextState = AdversialSearch::GreedySearch(game);
+            Output output(nextState);
+            break;
+        }
+        case 2:
+        {
+            vector<Node> traverseLog;
+            State nextState = AdversialSearch::MiniMax(game, traverseLog);
+            Output output(nextState, traverseLog);
+        }
+        case 3:
+        {
+            vector<AlphaBetaNode> traverseLog;
+            State nextState = AdversialSearch::AlphaBetaSearch(game, traverseLog);
+            Output output(nextState, traverseLog);
+        }
+        default:
+            break;
+    }
+    return 0;
+}
